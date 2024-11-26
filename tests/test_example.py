46f020ca2b50c4f74abcb7a1d2de6f9bf4b6f388
oleg <oleg.golubev74@gmail.com>
2024-11-26 13:27:23 +0300

    проверка через фикстуры

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,7 +1,17 @@
 from hexlet_pytest.example import reverse
 
 def test_reverse():
-    assert reverse('Hexlet') == 'telxeH'
+    #text_to_test_path = 'fixtures/text_to_test.txt'
+    with open("tests/fixtures/text_for_test.txt", encoding='utf8') as f:
+        text_to_test = f.read()
 
-def test_reverse_for_empty_string():
-    assert reverse('') == ''
+   # corr_answer_path = 'fixtures/corr_answer.txt'    
+    with open("tests/fixtures/corr_answer.txt", encoding='utf8') as f1:
+        corr_answer = f1.read()
+
+    #assert reverse('Hexlet') == 'telxeH'
+    assert reverse(text_to_test) == corr_answer
+
+
+#def test_reverse_for_empty_string():
+ #   assert reverse('') == ''
